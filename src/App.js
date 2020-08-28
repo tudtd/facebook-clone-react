8e572f93868f6ba8d079e9dbbc33b7679e912b6f
Tu Dinh <tu.dtd27@gmail.com>
2020-08-28 20:07:11 +0700

    init

---
+++ src/App.js
@@ -0,0 +1,19 @@
+import React from "react";
+import Header from "./components/Header";
+
+import "./App.css";
+
+const App = () => {
+    return (
+        <div className='app'>
+            <Header />
+
+            {/* App body  */}
+            {/* Sidebar */}
+            {/* Feed */}
+            {/* Contacts */}
+        </div>
+    );
+};
+
+export default App;
